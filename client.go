4be3f25b333eca1057c11969abf79d5a81604228
Paul Campbell <Paul.Campbell@bas.local>
2019-09-08 17:32:27 +0100

    Adding the client and clientInterface.go the purpose client.go sends requests to the server and prints out the end response

---
+++ client.go
@@ -0,0 +1,119 @@
+package main
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"bytes"
+	"fmt"
+)
+
+const (
+	encryptURI = "http://localhost:8080/encrypt"
+	decryptURI = "http://localhost:8080/decrypt"
+	applicationType = "application/json"
+)
+
+type encryptionRequest struct {
+	ID   string
+	Data string
+}
+
+type encryptionResponse struct {
+	Result string
+	Key    []byte
+}
+
+type EncrypterClient struct {
+  id string
+  payload []byte  
+}
+
+func (e EncrypterClient) Store(id, payload []byte) (aesKey []byte, err error) {
+	encryptionReq := encryptionRequest{}
+	encryptionReq.ID = string(id)
+	encryptionReq.Data = string(payload)
+
+	reqBody, err := json.Marshal(encryptionReq)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := http.Post(encryptURI, applicationType, bytes.NewBuffer(reqBody))
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+
+	respBody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var response encryptionResponse
+	err = json.Unmarshal(respBody, &response)
+	if err != nil {
+		return nil, err
+	}
+
+	return response.Key, nil
+}
+
+type decryptionRequest struct {
+	ID  string
+	Key []byte
+}
+
+type decryptionResponse struct {
+	Result string
+	Data   string
+}
+
+func (e EncrypterClient) Retrieve(id, aesKey []byte) (payload []byte, err error) {
+	decryptionReq := decryptionRequest{}
+	decryptionReq.ID = string(id)
+	decryptionReq.Key = aesKey
+
+	reqBody, err := json.Marshal(decryptionReq)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := http.Post(decryptURI, applicationType, bytes.NewBuffer(reqBody))
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+	respBody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var response decryptionResponse
+	err = json.Unmarshal(respBody, &response)
+	if err != nil {
+		return nil, err
+	}
+
+	return []byte(response.Data), nil
+}
+
+
+func main(){
+	var clinet Client
+	clinet = EncrypterClient{}
+	
+	aesKey, err := clinet.Store([]byte("1"), []byte("Yoti"))
+	if err != nil {
+		fmt.Printf("Error while trying to encrypt: %v", err)
+	}
+
+	plainText, err := clinet.Retrieve([]byte("1"), aesKey)
+    if err != nil {
+		fmt.Printf("Error while trying to decrypt: %v", err)
+	}
+	
+	fmt.Printf("Response ... Plain Text: %v", string(plainText))
+}
